873bceb27800daeb59013ad38424a6974b933d0b
bumang <umangabhattarai11@gmail.com>
2024-07-04 20:39:07 +0545

    fix: fix the flow of animation on menu page transition

--- src/features/feature-project/routes/FeatureProject.tsx
+++ src/features/feature-project/routes/FeatureProject.tsx
@@ -1,3 +1,13 @@
 import React from 'react';
+import { useGSAP } from '@gsap/react';
 
-export const FeatureProject = () => <div>FeatureProject</div>;
+import { useTransitionContext } from '@/context';
+
+export const FeatureProject = () => {
+  const { timeline } = useTransitionContext();
+
+  useGSAP(() => {
+    timeline.call(() => {}, undefined, 0);
+  }, {});
+  return <div>Project Page</div>;
+};
