ea9570f86f6d7a6adebc0006a1b81b925b9e9e72
Umanga Bhattarai <umangabhattarai@Umangas-MacBook-Pro.local>
2024-07-08 13:57:30 +0545

    chore: remove the console log

--- src/components/ui/Transition/index.tsx
+++ src/components/ui/Transition/index.tsx
@@ -36,14 +36,10 @@ const Transition = ({ children }: TransitionProps) => {
       ) {
         // Play the initial timeline and wait for it to complete
         if (!routeFromMenu) {
-          // eslint-disable-next-line no-console
-          console.log('children changed inside the timeline');
           await timeline.play().then(() => {
             timeline.pause().clear();
           });
         }
-        // eslint-disable-next-line no-console
-        console.log('children changed outside the timeline');
 
         // Define the firstPageTransTimeline animation
         firstPageTransTimeline.to(
