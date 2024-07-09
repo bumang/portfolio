9dc45323a37a00aad0f9831f94574b5d8c49b36f
Umanga Bhattarai <umangabhattarai@Umangas-MacBook-Pro.local>
2024-07-09 13:27:06 +0545

    feat: implement horizontal scrolling in about page

--- src/layouts/about/AboutPageLayout.tsx
+++ src/layouts/about/AboutPageLayout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MenuDrawer, TopHeader } from '@/components';
+import { MenuDrawer } from '@/components';
 import { useMyContext } from '@/context';
 
 interface AboutLayoutProps {
@@ -10,13 +10,8 @@ interface AboutLayoutProps {
 export const AboutPageLayout = ({ children }: AboutLayoutProps) => {
   const { menuIsOpen, setMenuIsOpen } = useMyContext();
   return (
-    <div className="min-w-screen relative min-h-screen overflow-hidden bg-background-about">
-      <div className="relative h-screen justify-center overflow-x-hidden pt-s88">
-        <div className="fixed top-0 z-[70] h-s88 w-full">
-          <TopHeader />
-        </div>
-        {children}
-      </div>
+    <div className="relative h-screen w-screen">
+      {children}
       <MenuDrawer menu={menuIsOpen} setMenu={setMenuIsOpen} />
     </div>
   );
