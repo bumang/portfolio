cdc3e65979f0dd430b8fbebbf9ced34c1d345b7b
Umanga <umangabhattarai11@gmail.com>
2024-07-17 01:20:48 +0545

    feat: implement mountain parallel animation effect

--- src/components/common/CustomCursor/index.tsx
+++ src/components/common/CustomCursor/index.tsx
@@ -4,13 +4,17 @@ import React, { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 
+import { useMyContext } from '@/context';
+
 export const CustomCursor = () => {
   const cursorRef = useRef<HTMLDivElement>(null);
+  const { setMousePosition } = useMyContext();
 
   useGSAP(
     () => {
       const moveCursor = (event: MouseEvent) => {
         if (cursorRef.current) {
+          setMousePosition({ x: event.clientX - 8, y: event.clientY - 840 });
           gsap.to(cursorRef.current, {
             duration: 0.3,
             ease: 'power3.out',
@@ -36,7 +40,11 @@ export const CustomCursor = () => {
     <div
       className="cursor z-[99] h-s16 w-s16 rounded-full border-2 border-solid bg-background-white"
       ref={cursorRef}
-      style={{ position: 'fixed', pointerEvents: 'none', transform: 'translate3d(0, 0, 0)' }}
+      style={{
+        position: 'fixed',
+        pointerEvents: 'none',
+        transform: 'translate3d(mousePosition.x, mousePosition.y, 0)',
+      }}
     />
   );
 };
