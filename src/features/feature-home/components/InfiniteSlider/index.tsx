df59da8aae715268bde527c37d8ccd3aea67fa4c
Umanga <umangabhattarai11@gmail.com>
2024-07-16 18:01:02 +0545

    fix: bottom position of mountain

--- src/features/feature-home/components/InfiniteSlider/index.tsx
+++ src/features/feature-home/components/InfiniteSlider/index.tsx
@@ -5,7 +5,7 @@ interface InfiniteSliderProps extends ImageProps {}
 
 const InfiniteSlider = ({ src, alt, width, height, fill }: InfiniteSliderProps) => (
   <div className="absolute bottom-0 left-0 h-full w-full before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,black_0%,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,black_0%,rgba(255,255,255,0)_100%)] after:content-['']">
-    <div className="min-w-screen lg:min-w-screen absolute left-0 h-[120vh] w-full xs:bottom-[-55%] md:bottom-[-50%] lg:bottom-[-48%] xl:bottom-[-40%]">
+    <div className="min-w-screen lg:min-w-screen absolute left-0 h-[120vh] w-full xs:bottom-[-55%] md:bottom-[-50%] lg:bottom-[-48%] xl:bottom-[-38%]">
       <Image
         src={`${process.env.NEXT_PUBLIC_PATH_PREFIX ?? ''}/mountain.svg`}
         alt="mountain"
