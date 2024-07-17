a20eadf531fee06f7c1fe7daad1c6c44d1aac67a
Umanga <umangabhattarai11@gmail.com>
2024-07-17 11:09:43 +0545

    fix: change image format of mountain

--- src/features/feature-about/routes/FeatureAbout.tsx
+++ src/features/feature-about/routes/FeatureAbout.tsx
@@ -5,11 +5,10 @@ import { SectionScrollFirst } from '@/features/feature-about/components';
 
 export const FeatureAbout = () => {
   return (
-    <div className="flex h-full w-screen flex-col overflow-y-scroll bg-background-about">
+    <div className="no-scrollbar flex h-full flex-col overflow-auto bg-background-about">
       <div className="fixed top-0 z-[70] h-s88 w-full">
         <TopHeader />
       </div>
-      <div className="screen min-w-screen min-h-screen bg-red-500">Hello</div>
       <SectionScrollFirst />
       <div className="screen min-w-screen min-h-screen bg-red-500">Hello</div>
     </div>
