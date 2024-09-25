b720347538a46739e007a4441829c5c00b145b5c
Umanga <umangabhattarai11@gmail.com>
2024-09-25 17:09:12 +0545

    fix: cursor position

--- src/components/common/CustomCursor/index.tsx
+++ src/components/common/CustomCursor/index.tsx
@@ -19,7 +19,7 @@ export const CustomCursor = () => {
             duration: 0.3,
             ease: 'power3.out',
             x: event.clientX - 8,
-            y: event.clientY - 840,
+            y: event.clientY - 792,
             force3D: true, // Ensure 3D rendering
           });
         }
